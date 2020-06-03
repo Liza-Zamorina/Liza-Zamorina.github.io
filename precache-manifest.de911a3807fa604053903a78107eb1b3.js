62b7a81854ab5548407519df36de61a80d953ced
Liza Zamorina <lizazamorina2001@gmail.com>
2020-06-03 14:10:27 +0300

    init and done

---
+++ precache-manifest.de911a3807fa604053903a78107eb1b3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfe58d963014153a110025cdf8813fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef53f724ebec58c5b746",
+    "url": "/static/css/2.d5e9ecad.chunk.css"
+  },
+  {
+    "revision": "927518562c76261c41a3",
+    "url": "/static/css/main.d0580158.chunk.css"
+  },
+  {
+    "revision": "ef53f724ebec58c5b746",
+    "url": "/static/js/2.b3c2396c.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.b3c2396c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927518562c76261c41a3",
+    "url": "/static/js/main.eef5ade7.chunk.js"
+  },
+  {
+    "revision": "bb81a0ee33ad5f8a91d9",
+    "url": "/static/js/runtime-main.8f2ec1c0.js"
+  },
+  {
+    "revision": "9b752b8dcf3bc6f780aef699081282c8",
+    "url": "/static/media/bg.9b752b8d.jpg"
+  },
+  {
+    "revision": "9eb5fd57616b1b4ab3fe49d6d128fa6e",
+    "url": "/static/media/calendar-icon.9eb5fd57.svg"
+  },
+  {
+    "revision": "7a4c4ed3e537d3fb59c7decd495bfec7",
+    "url": "/static/media/cancel.7a4c4ed3.svg"
+  },
+  {
+    "revision": "bcb354fea7986c9a99971563498453b1",
+    "url": "/static/media/document-icon.bcb354fe.svg"
+  },
+  {
+    "revision": "aa922b9a44fd3421002f8beaf817f62e",
+    "url": "/static/media/plus.aa922b9a.svg"
+  },
+  {
+    "revision": "924a20cb34e94e66c0e214ae78931725",
+    "url": "/static/media/search-icon.924a20cb.svg"
+  },
+  {
+    "revision": "1f224c628b4fc27880badf3b1e182a94",
+    "url": "/static/media/settings-icon.1f224c62.svg"
+  },
+  {
+    "revision": "71a9bcdf1d2cf9e4cabcfb6e0caf1957",
+    "url": "/static/media/task-icon.71a9bcdf.svg"
+  }
+]);
